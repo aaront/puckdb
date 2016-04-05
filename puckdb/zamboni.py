f1ae25138a5cfac9e74e6342aea10d2248a8f8e2
Aaron Toth <atoth89@gmail.com>
2016-04-04 23:13:28 -0400

    Something done, but alas no closer

--- puckdb/zamboni.py
+++ puckdb/zamboni.py
@@ -1,20 +1,34 @@
 import abc
 from datetime import datetime
 
+import asyncio
 import aiohttp
 
 from . import exceptions, filters
 
+
 class BaseScraper(object):
     __metaclass__ = abc.ABCMeta
 
+    def __init__(self, loop: asyncio.BaseEventLoop):
+        self.loop = loop
+
     @abc.abstractmethod
-    async def get_schedule(self, game_filter: filters.GameFilter):
+    async def process(self, data):
+        pass
+
+    @abc.abstractmethod
+    async def get(self, filter_by: filters.BaseFilter):
         pass
 
 
-class NHLScraper(BaseScraper):
-    schedule_url = 'http://live.nhl.com/GameData/SeasonSchedule-{season}.json'
+class NHLGameScraper(BaseScraper):
+    url = 'https://statsapi.web.nhl.com/api/v1/schedule?startDate={from_date}&endDate={to_date}' \
+          '&expand=schedule.teams,schedule.linescore,schedule.broadcasts.all,schedule.ticket,schedule.game.content' \
+          '.media.epg&leaderCategories=&site=en_nhl&teamId='
 
-    async def get_schedule(self, game_filter: filters.GameFilter):
+    async def process(self, data):
         pass
+
+    async def get(self, filter_by: filters.GameFilter):
+        super().get(filter_by)
