bab5089d653ed9321d08dced9c88bf17f8bcc8e6
Aaron Toth <atoth89@gmail.com>
2016-04-29 08:33:23 -0400

    bikeshedding

--- puckdb/scrapers.py
+++ puckdb/scrapers.py
@@ -3,25 +3,25 @@ import abc
 from . import filters
 
 
-class BaseProcessor(object):
+class BaseScraper(object):
     __metaclass__ = abc.ABCMeta
 
     @abc.abstractmethod
-    def process(self, data):
+    async def process(self, data):
         pass
 
     @abc.abstractmethod
-    def get_urls(self, filter_by: filters.BaseFilter):
+    async def run(self, filter_by: filters.BaseFilter):
         pass
 
 
-class NHLGameProcessor(BaseProcessor):
+class NHLGameScraper(BaseScraper):
     url = 'https://statsapi.web.nhl.com/api/v1/schedule?startDate={from_date}&endDate={to_date}' \
           '&expand=schedule.teams,schedule.linescore,schedule.broadcasts.all,schedule.ticket,schedule.game.content' \
           '.media.epg&leaderCategories=&site=en_nhl&teamId='
 
-    def process(self, data):
+    async def process(self, data):
         pass
 
-    def get_urls(self, filter_by: filters.GameFilter):
+    async def run(self, filter_by: filters.GameFilter):
         return [self.url.format(from_date=day, to_date=day) for day in filter_by.intervals]
