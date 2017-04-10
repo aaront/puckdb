ced9d3a0ed96f3662937e4c0059770d97b89d6a9
Aaron Toth <atoth89@gmail.com>
2017-04-10 00:23:07 -0400

    Proper URLs

--- puckdb/extern/nhl.py
+++ puckdb/extern/nhl.py
@@ -11,19 +11,19 @@ _HEADERS = {
 _BASE_URL = 'https://statsapi.web.nhl.com/api/v1'
 
 async def get_teams(session: aiohttp.ClientSession):
-    return await _fetch('/teams', session)
+    return await _fetch(_BASE_URL + '/teams', session)
 
 async def get_schedule_games(from_date: datetime, to_date: datetime, session: aiohttp.ClientSession):
     url = '/schedule?startDate={from_date}&endDate={to_date}&expand=schedule&site=en_nhl&teamId='.format(
         from_date=from_date.strftime('%Y-%m-%d'),
         to_date=to_date.strftime('%Y-%m-%d')
     )
-    schedule = await _fetch(url, session)
+    schedule = await _fetch(_BASE_URL + url, session)
     return list(itertools.chain.from_iterable([day['games'] for day in schedule['dates']]))
 
 async def get_live_data(game_id: int, session: aiohttp.ClientSession):
     url = '/game/{game_id}/feed/live'.format(game_id=game_id)
-    return await _fetch(url, session)
+    return await _fetch(_BASE_URL + url, session)
 
 async def _fetch(url: str, session: aiohttp.ClientSession):
     async with session.get(url, headers=_HEADERS) as response:
