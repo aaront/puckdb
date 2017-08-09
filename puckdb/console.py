fda3ba8d67c33a623858b187842c8fff2c4f8070
Aaron Toth <atoth89@gmail.com>
2017-08-09 18:30:35 -0400

    Fix and improve console

--- puckdb/console.py
+++ puckdb/console.py
@@ -1,7 +1,9 @@
 import asyncio
 import sys
+import os
 from datetime import datetime
 
+import asyncpg.exceptions
 import click
 import click_datetime
 
@@ -40,15 +42,19 @@ def drop():
 @click.option('--from-date', type=DATE_PARAM, default=datetime(2016, 10, 1))
 @click.option('--to-date', type=DATE_PARAM, default=datetime.now())
 def get(from_date, to_date):
-    games = loop.run_until_complete(fetch.get_games(from_date=from_date, to_date=to_date, loop=loop))
-    click.echo(len(games))
+    try:
+        games = loop.run_until_complete(fetch.get_games(from_date=from_date, to_date=to_date, loop=loop))
+        click.echo(len(games))
+    except asyncpg.exceptions.UndefinedTableError:
+        click.echo('ERROR: Please run `puckdb init` to initialize this DB first.')
+        sys.exit(1)
 
 
 @click.group()
 @click.version_option()
 def main():
-    if db.connect_str is None:
-        click.echo('ERROR: `PUCKDB_DATABASE` environment variable not specified.')
+    if not os.getenv('PUCKDB_DB_DATABASE'):
+        click.echo('ERROR: `PUCKDB_DB_DATABASE` environment variable not specified.')
         sys.exit(1)
     _setup()
 
