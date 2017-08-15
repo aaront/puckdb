79388d86c1f9196bcb6d53786925738c2928259e
Aaron Toth <atoth89@gmail.com>
2017-08-14 23:57:43 -0400

    Python 3.6 only yo

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
         'Natural Language :: English',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries'
     )
 )
