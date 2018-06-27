453962c547faec68ae47aa54f7bef1af7fb5c62f
Aaron Toth <atoth89@gmail.com>
2018-06-26 21:58:54 -0400

    Update trove Python versioning

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
         'Natural Language :: English',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Software Development :: Libraries'
     )
 )
