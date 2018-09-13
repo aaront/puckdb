f3be1ea72ca0f921c19880732cffb2b2628bae8e
Aaron Toth <atoth89@gmail.com>
2018-09-12 22:52:24 -0400

    Added start of parsers test

--- tests/test_fetch.py
+++ tests/test_fetch.py
@@ -1,6 +1,5 @@
 import asyncio
 import os
-import uuid
 from datetime import datetime
 
 import pytest
---
+++ tests/test_parsers.py
@@ -0,0 +1,17 @@
+import json
+import os
+import pathlib
+import pytest
+
+from puckdb import parsers
+
+game_data_path = pathlib.Path(__file__).parent.joinpath('data')
+game_2016_id = 2016021207
+with game_data_path.joinpath(f'game_{game_2016_id}.json').open('r') as f:
+    game_2016 = json.load(f)
+
+
+class TestParsers:
+    def test_game(self):
+        game = parsers.game(game_2016_id, 1, game_2016)
+        assert game.id == 2016021207
