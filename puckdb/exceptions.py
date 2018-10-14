077ef278732b98ff9729b84643cceb18566ba440
Aaron Toth <atoth89@gmail.com>
2018-10-13 23:15:44 -0400

    Removed unused exceptions module

--- puckdb/exceptions.py
+++
@@ -1,7 +0,0 @@
-class FilterException(Exception):
-    def __init__(self, message=None):
-        self.message = message
-
-    def __str__(self):
-        filter_message = f': {self.message}' if self.message else ''
-        return f'Invalid filter{filter_message}'
